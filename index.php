50a7d08d34ee30b907f323cda21bfdfa9b9b18c9
house_pc <19307048@utfv.edu.mx>
2023-04-18 22:39:56 -0600

    avance de codigo de barras

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <title>Document</title>
+</head>
+<body>
+    
+
 <?php
 
 
@@ -8,9 +21,17 @@ while($result=$cs -> fetchArray()){
    
 
 ?>
+<div class="card mx-auto bg-light" style="width: 20rem; text-align: center; ">
+    <div class="card-body">
+        <img src="img/LOGO_TUVSA.svg" style="text-align: left;"/> 
+        <br/>
+        <br/>
 
-<img src="barcode.php?text=<?php echo $result['noUnidad'];?>&size=50&codetype=Code39&print=true&sizefactor=2" />.<br>
 
+        <img src="barcode.php?text=<?php echo $result['noUnidad'];?>&size=50&codetype=Code39&print=true&sizefactor=2" style="text-align: left;"/><br>
+
+    </div>
+</div>
 
 <?php
 }
@@ -25,3 +46,5 @@ while($row = $cs ->fetch_assoc()){ -->
 
 
 
+    </body>
+</html>
